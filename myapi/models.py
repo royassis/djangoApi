be95732b14872c8330411663e2815cfba638c887
Roy <royassisg@gmail.com>
2021-03-20 13:32:33 +0200

    cmt

---
+++ myapi/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Hero(models.Model):
+    name = models.CharField(max_length=120)
+    alias = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
