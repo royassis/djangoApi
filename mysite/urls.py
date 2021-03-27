c4a82ec9edb0712b81d29ae14f4160e7d71783a0
Roy <royassisg@gmail.com>
2021-03-27 17:09:55 +0300

    msg

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""djangoApi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapi.urls')),
+    path('api/', include('myapi.urls')),
+    path('', include('models_page.urls')),
  ]
