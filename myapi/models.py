9a96022b040ceea5a7bf19ffc2c8490d51cc5219
User <royassisg@gmail.com>
2021-03-30 12:18:03 +0300

    Change serilizers

--- myapi/models.py
+++ myapi/models.py
@@ -11,7 +11,7 @@ class MlProject(models.Model):
 class MlModel(models.Model):
     project = models.ForeignKey(MlProject, on_delete=models.CASCADE, related_name="mlmodels")
     name = models.CharField(max_length=120)
-    # model = models.BinaryField()
+    model = models.BinaryField(null=True)
     upload = models.FileField(upload_to='message/%Y/%m/%d/', null=True, blank=True)
 
     def __str__(self):
--- myapi/serializers.py
+++ myapi/serializers.py
@@ -7,22 +7,11 @@ from .models import MlModel, MlProject
 class MlModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = MlModel
-        fields = ('id','name', 'upload')
+        fields = ('id','project','name', 'upload')
 
 class MlProjectSerializer(serializers.ModelSerializer):
-    mlmodels = MlModelSerializer(many=True)
+    mlmodels = MlModelSerializer(many=True, read_only=True)
 
     class Meta:
         model = MlProject
-        fields = ("id","name",'mlmodels')
-
-    def create(self, validated_data):
-        print("--"*20)
-        mlmodels_data = validated_data.pop("mlmodels")
-        mlproj = MlProject(**validated_data)
-        mlproj.save()
-        for mlmodel_data in mlmodels_data:
-            mm = mlproj.mlmodels.create(**mlmodel_data)
-            mm.save()
-
-        return mlproj
+        fields = ("id","name","mlmodels")
