c17c4f4530c1ee57fea722d640e7c85a9502d7d7
Roy <royassisg@gmail.com>
2021-03-30 09:28:18 +0300

    Update example

--- myapi/examples/api_request_example.py
+++ myapi/examples/api_request_example.py
@@ -1,8 +1,9 @@
-import requests
-import io
 import pickle
 
+import requests
+
 file = open(r'C:\Users\Roy\djangoProjects\djangoApi\myapi\examples\api_request_example.ps1', "wb+")
+files = {"filename": file}
 pickled_obj = pickle.dumps("Asd")
 
 payload = {'name': "mynewmodel", 'model': pickled_obj}
@@ -13,6 +14,6 @@ payload = {
 auth = ('roy', '1234')
 url = "http://localhost:8000/api/mlproject/"
 
-r = requests.post(url=url, json=payload, auth=auth)
+r = requests.post(url=url, json=payload, auth=auth, files=files)
 
 print(r.status_code)
