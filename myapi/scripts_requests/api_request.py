c21a666ec02c87938699fa54513c52996801b5d8
Roy <royassisg@gmail.com>
2021-04-13 21:32:41 +0300

    Fix file upload request

--- myapi/scripts_requests/api_request.py
+++ myapi/scripts_requests/api_request.py
@@ -1,10 +1,14 @@
 import requests
 
-file = open(r'C:\Users\Roy\djangoProjects\djangoApi\myapi\scripts_requests\api_get_list.py', "wb+")
-files = {"file": file}
-
-payload = {'project': 25, 'name': "model_i"}
+url = "http://localhost:8000/api/mlproject/"
 auth = ('roy', '1234')
+
+r = requests.get(url=url, auth=auth)
+project_id = int(r.json()[0].get("id"))
+
+file = open(r'C:\Users\Roy\djangoProjects\djangoApi\myapi\scripts_requests\api_request.py', "rb")
+files = {"upload": file}
+payload = {'project': project_id, 'name': "model_new"}
 url = "http://localhost:8000/api/mlmodels/"
 
 r = requests.post(url=url, data=payload, auth=auth, files=files)
